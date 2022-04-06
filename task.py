b110d4bfa1c5d9ce8eba07e9b7904a0ce89a0b63
Semen Evelkin <semen.evelkin@backer-stream.com>
2022-04-06 21:31:00 +0300

    edit class

--- task.py
+++ task.py
@@ -1,65 +1,96 @@
-class Serie:
-	"""
-		Начиная с Python v3 все классы явно и неявно наследуются от базового
-	   	класса object
-	"""
+# Since Python v3, all classes are explicitly
+# and implicitly inherited from the base class object
 
-	def __new__(cls, *args, **kwargs):
-		"""Вызывается перед созданикм класса"""
-		return super().__new__(cls)
+class Series:
+    """custom class for test"""
+    KEY = 1492
 
-	def __init__(self, *args):
-		"""Инициализатор объектов класса"""
-		self.args = [*args]
+    @classmethod
+    def __check_value(cls, value):
+        return type(value) in (int, float) and 0 <= value != cls.KEY
 
-	def __del__(self):
-		"""Финализатор класса"""
-		pass
+    def __new__(cls, *args, **kwargs):
+        """before init class"""
+        return super().__new__(cls)
 
-	def __repr__(self):
-		"""Class"""
-		return 'Serie()'
+    def __init__(self, *args):
+        """init class"""
+        self.args = [*args]
 
-	def __str__(self):
-		"""Values"""
-		return str(self.args)
+    def __del__(self):
+        """finalize class"""
+        pass
 
-	def __iter__(self):
-		return self
+    def __repr__(self):
+        """Class"""
+        return str(self.args)
 
-	def lenght(self):
-		"""
-		Count items in the series
-		"""
-		count = 0
-		for i in self.args:
-			count += 1
-		return count
+    def __str__(self):
+        """Values"""
+        return str(self.args)
 
-	def add(self, value):
-		"""
-		Append new item in end of the series
-		"""
-		self.args = self.args + [value]
-		return self.args
+    def __iter__(self):
+        return self
 
-	def update(self, *args):
-		"""
-		Append a few item in end of the series
-		"""
-		self.args = self.args + [*args]
-		return self.args
+    def length(self):
+        """
+        Count items in the series
+        """
+        count = 0
+        for i in self.args:
+            count += i
+        return count
 
-	def remove(self, value):
-		"""
-		Find and remove item in the series
-		"""
-		new_series = [] 
-		for i in self.args:
-			if i != value:
-				new_series = new_series + [i]
-		if new_series == self.args:
-			return f'{value} - not found into this series'
-		else:
-			return new_series
+    def add(self, value=None):
+        """
+        Append new item in end of the series
+        """
+        if self.__check_value(value):
+            self.args = self.args + [value]
+        return self.args
 
+    def update(self, *args):
+        """
+        Append a few item in end of the series
+        """
+        self.args = self.args + [*args]
+        return self.args
+
+    def remove(self, value):
+        """
+        Find and remove item in the series
+        """
+        new_series = []
+        for i in self.args:
+            if i != value:
+                new_series = new_series + [i]
+        if new_series == self.args:
+            return f'{value} - not found into this series'
+        else:
+            return new_series
+
+    def sum(self):
+        """
+        Counts the sum of the sequence
+        """
+        print(self.summa(self.args))
+
+    def avg(self):
+        """
+        Counts the average value of the sequence
+        """
+        print(self.average(self.args))
+
+    @staticmethod
+    def summa(args):
+        s = 0
+        for i in args:
+            s += i
+        return s
+
+    @staticmethod
+    def average(args):
+        s = 0
+        for i in args:
+            s += i
+        return s / len(args)
